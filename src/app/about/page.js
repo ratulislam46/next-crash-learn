1189ccd7e58f51b91c2d2ecf92fea587d3c1bdf6
ratulislam46 <arfanratul46@gmail.com>
2025-07-31 22:45:56 +0600

    uses static metadata

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,13 +2,19 @@ import Image from 'next/image';
 import React from 'react';
 import nature from '@/public/images/nature.jpg'
 
+
+export const metadata = {
+    title: "About",
+    description: "Mission, vission, fission routes here",
+};
+
 const About = () => {
     // throw new Error('error')
     return (
         <div>
             <h1>This Is About Page</h1>
 
-            <Image src={nature} alt='nature'/>
+            <Image src={nature} alt='nature' />
 
         </div>
     );
